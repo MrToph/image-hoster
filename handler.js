e19176f072823b51ead894d886e65ea306dc34fe
Christoph Michel <christoph.michel.1992@gmail.com>
2018-05-21 21:47:49 +0200

    Implement CORS handling. No overwrite of existing images

--- handler.js
+++ handler.js
@@ -1,27 +1,54 @@
+"use strict";
 
-'use strict'
+const AWS = require("aws-sdk");
+const uuidv4 = require("uuid/v4");
 
-const AWS = require('aws-sdk');
+const allowedOrigins = ['http://localhost:3000', 'https://kingofeos.com']
 
 module.exports.requestUploadURL = (event, context, callback) => {
   const s3 = new AWS.S3();
   const params = JSON.parse(event.body);
+  const origin = event.headers.origin
+  if(!allowedOrigins.some(o => o === origin)) {
+    return callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({ error: `Your domain "${origin}" is not allowed to access this resource.` })
+    });
+  }
 
+  const fileName = `${uuidv4()}-${params.name}`;
   const s3Params = {
-    Bucket: 'image-hoster',
-    Key:  params.name,
-    ContentType: params.type,
-    ACL: 'public-read',
+    Bucket: "image-hoster",
+    Key: fileName
   };
 
-  const uploadURL = s3.getSignedUrl('putObject', s3Params);
+  s3.headObject(s3Params, function(err, metadata) {
+    if (err && err.code === "NotFound") {
+      // here image has not been uploaded already, because we got an error.code 'NotFound'
+      const s3UploadParams = Object.assign(
+        {
+          ContentType: params.type,
+          ACL: "public-read"
+        },
+        s3Params
+      );
+      const uploadURL = s3.getSignedUrl("putObject", s3UploadParams);
 
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      // TODO: change this for production
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({ uploadURL: uploadURL }),
-  })
-}
+      return callback(null, {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": `${origin}`
+        },
+        body: JSON.stringify({ uploadURL: uploadURL, fileName: fileName })
+      });
+    } else {
+      return callback(null, {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": `${origin}`
+        },
+        body: JSON.stringify({ error: err || `Object already exists` })
+      });
+    }
+  });
+};
